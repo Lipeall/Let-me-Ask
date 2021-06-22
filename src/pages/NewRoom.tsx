1917e0d0dce0ac651ad3c0dec8c964e4f15408d0
Filipe Almeida <Filipe-all@hotmail.com>
2021-06-22 20:35:17 -0300

    Add authentication firebase

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -21,7 +23,7 @@ export function NewRoom() {
                         <input type="text" placeholder="Nome da sala" />
                         <Button type="submit">Criar sala</Button>
                     </form>
-                    <p>Quer entrar em uma sala existente?<a href="#">Clique aqui</a></p>
+                    <p>Quer entrar em uma sala existente?<Link to="/">Clique aqui</Link></p>
                 </div>
             </main>
         </div>
