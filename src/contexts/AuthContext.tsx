f3f4de9afebd98fba3c7ce8d108d392490cc4631
Filipe Almeida <Filipe-all@hotmail.com>
2021-06-24 16:50:57 -0300

    add room

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -21,38 +21,13 @@ export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthContextProvider(props: AuthContextProviderProps) {
 
-    const [user, setUser] = useState<User>();
+  const [user, setUser] = useState<User>();
 
-    useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged(user => {
-        if (user) {
-          const { displayName, photoURL, uid } = user
-  
-          if (!displayName || !photoURL) {
-            throw new Error('Missing information from Google Account.');
-          }
-    
-          setUser({
-            id: uid,
-            name: displayName,
-            avatar: photoURL
-          })
-        }
-      })
-  
-      return () => {
-        unsubscribe();
-      }
-    }, [])
-  
-    async function signInWithGoogle() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-  
-      const result = await auth.signInWithPopup(provider)
-  
-      if (result.user) {
-        const { displayName, photoURL, uid } = result.user
-  
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user
+
         if (!displayName || !photoURL) {
           throw new Error('Missing information from Google Account.');
         }
@@ -63,10 +38,35 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
           avatar: photoURL
         })
       }
+    })
+
+    return () => {
+      unsubscribe();
     }
-    return (
-        <AuthContext.Provider value={{ user, signInWithGoogle }}>
-            {props.children}
-        </AuthContext.Provider>
-    );
+  }, [])
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider)
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user
+
+      if (!displayName || !photoURL) {
+        throw new Error('Missing information from Google Account.');
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      })
+    }
+  }
+  return (
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+          {props.children}
+      </AuthContext.Provider>
+  );
 }
